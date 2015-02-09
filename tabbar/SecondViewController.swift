c9fabe168cbc5ce09b46908da9599923ea0f3b02
videmor <videmor@gmail.com>
2015-02-08 21:36:32 -0500

    Initial Commit

---
+++ tabbar/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  tabbar
+//
+//  Created by Jorge Crisóstomo Palacios on 2/8/15.
+//  Copyright (c) 2015 videmor. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
